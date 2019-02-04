17fba5f143e53c44e696780009dad608be4dc309
Bhanu V <vadlamudi.bhanu@gmail.com>
2019-02-04 00:00:24 -0500

    app test

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ app = Flask(__name__)   # Flask constructor
   
 @app.route('/')       
 def index(): 
-    return 'HELLO world! spilting the ansible testt!'
+    return 'HELLO world! spilting..... the ansible testt!'
   
 if __name__=='__main__': 
-   app.run(host='0.0.0.0', port=5050)
+   app.run(host='0.0.0.0', port=5051)
    
