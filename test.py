9fb0f2f19e31449ca6f16e36972c2aa5d7a238a8
Bhanu V <vadlamudi.bhanu@gmail.com>
2019-02-12 00:06:39 -0500

    changes

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ app = Flask(__name__)   # Flask constructor
 @app.route('/')       
 def index(): 
     return 'Hello World!!checking the scalibiltyyyyy and automation......'
-init __name__=='__main__': 
+if __name__=='__main__': 
    app.run(host='0.0.0.0', port=8080)
    
